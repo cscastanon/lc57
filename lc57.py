2a8ff13f8834dfd13498d11ca5ee34d34bba373f
cscastanon <120608209+cscastanon@users.noreply.github.com>
2023-05-29 20:31:21 -0500

    Add files via upload

---
+++ lc57.py
@@ -0,0 +1,18 @@
+#Leetcode #57
+# Time Complexity: O(n) => we iterate through our list of intervals once, for at most n elements
+# Space Complexity: O(n) => we are store a new list of intervals with n elements
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
+        return res
